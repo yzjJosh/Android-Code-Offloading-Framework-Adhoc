c6dcc95e54d98d64487d7909f78af6f41d865088
yz9234 <LiviaZhao@wireless-10-145-109-188.public.utexas.edu>
2016-10-12 22:41:43 -0500

    Server adapt API change in Library.

--- Server/app/src/main/java/mobileclould/android/server/service/ServerThread.java
+++ Server/app/src/main/java/mobileclould/android/server/service/ServerThread.java
@@ -2,15 +2,15 @@ package mobileclould.android.server.service;
 
 import android.util.Log;
 
-import java.io.ObjectInputStream;
+import java.io.InputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import mobilecloud.api.Request;
 import mobilecloud.api.Response;
 import mobilecloud.server.Server;
 
@@ -63,20 +63,19 @@ public class ServerThread extends Thread {
         @Override
         public Response call() throws Exception {
             try {
-                ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-                Request request = (Request) is.readObject();
-                Log.d(TAG, "Got request type: " + request.getClass().getSimpleName());
-                Response resp = Server.getInstance().serve(request);
+                Log.d(TAG, "Got request");
+
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+                Response resp = Server.getInstance().serve(is, os);
+
                 if(resp.isSuccess()) {
-                    Log.d(TAG, "Complete serving request " + request.getClass().getSimpleName() +
+                    Log.d(TAG, "Complete serving request, response is " + resp.getClass().getSimpleName() +
                             ", status is success.");
                 } else {
-                    Log.d(TAG, "Compelte serving request" + request.getClass().getSimpleName() +
+                    Log.d(TAG, "Compelte serving request, response is " + resp.getClass().getSimpleName() +
                             ", status is failed, reason is " + resp.getThrowable());
                 }
-                os.writeObject(resp);
-                os.flush();
                 return resp;
             } catch (Exception e) {
                 e.printStackTrace();
