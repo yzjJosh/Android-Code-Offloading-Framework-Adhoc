8955dc0207359f550ed4dae7f10b1235b5b56d01
Josh <yangzijiangjosh@gmail.com>
2016-11-11 15:30:16 -0600

    Change schedular to least connection schedular

--- Transformer/Library/src/main/java/mobilecloud/engine/Config.java
+++ Transformer/Library/src/main/java/mobilecloud/engine/Config.java
@@ -1,11 +1,11 @@
 package mobilecloud.engine;
 
 import mobilecloud.engine.host.provider.CentralServerHostProvider;
-import mobilecloud.engine.schedular.RoundRobinSchedular;
+import mobilecloud.engine.schedular.LeastConnectionSchedular;
 
 public class Config {
     
-    public static final String SCHEDULAR = RoundRobinSchedular.class.getName();
+    public static final String SCHEDULAR = LeastConnectionSchedular.class.getName();
     public static final String HOST_PROVIDER = CentralServerHostProvider.class.getName();
     public static final long HOST_MONITOR_CHECK_PROVIDER_INTERVAL = 10000;
     public static final long HOST_MONITOR_CHECK_HOST_INTERVAL = 5000;
