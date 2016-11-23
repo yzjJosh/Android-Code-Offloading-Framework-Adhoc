a29f15bfa0fc95dad78adcf1fc63cb9a47b08b8f
yz9234 <LiviaZhao@wireless-10-145-203-134.public.utexas.edu>
2016-11-23 15:12:16 -0600

    change return type

--- examples/NQueenSolver/app/src/main/java/example/nqueensolver/NQueenSolver.java
+++ examples/NQueenSolver/app/src/main/java/example/nqueensolver/NQueenSolver.java
@@ -14,31 +14,31 @@ import java.util.concurrent.Future;
 
 public class NQueenSolver {
 
-    public List<List<Integer>> totalNQueens(int n) throws ExecutionException, InterruptedException {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public int totalNQueens(int n) throws ExecutionException, InterruptedException {
+        int result = 0;
         if(n<=0) return result;
 
         ExecutorService threadPool = Executors.newCachedThreadPool();
-        List<Future<List<List<Integer>>>> items = new ArrayList<>();
+        List<Future<Integer>> items = new ArrayList<>();
 
         for(int i = 0; i < n; i++) {
-            Future<List<List<Integer>>> item = threadPool.submit(new worker(i, n));
+            Future<Integer> item = threadPool.submit(new worker(i, n));
             items.add(item);
         }
 
         for(int i = 0; i < items.size(); i++) {
-            result.addAll(items.get(i).get());
+            result += items.get(i).get();
         }
         return result;
     }
 
-    private class worker implements Callable<List<List<Integer>>> {
+    private class worker implements Callable<Integer> {
 
-        public List<List<Integer>> res = new ArrayList<>();
         public boolean[][] board;
         public boolean[] occupied;
         public int i;
         public int n;
+        public int nums = 0;
 
         public worker(int i, int n) {
             this.board = new boolean[n][n];
@@ -47,50 +47,41 @@ public class NQueenSolver {
             this.n = n;
         }
 
-        public List<List<Integer>> call() {
+        public Integer call() {
             occupied[i] = true;
             board[0][i] = true;
-            helper(res, board, occupied, 1, n);
-            return res;
+            helper(board, occupied, 1, n);
+            return nums;
         }
-    }
 
-    public void helper(List<List<Integer>> res, boolean[][] board, boolean[] occupied, int row, int n){
-        if(row==n){
-            List<Integer> temp = new ArrayList<Integer>();
-            for(boolean[] item : board){
-              for(int i=0; i<item.length; i++) {
-                  if (item[i]) {
-                      temp.add(i);
-                      break;
-                  }
-              }
+        public void helper(boolean[][] board, boolean[] occupied, int row, int n){
+            if(row==n){
+                nums++;
+                return;
             }
-            res.add(temp);
-            return ;
+
+            for(int i=0; i<n; i++){
+                if(!isValid(board, occupied, row, i)) continue;
+                board[row][i] = true;
+                occupied[i] = true;
+                helper(board, occupied, row+1, n);
+                board[row][i] = false;
+                occupied[i] = false;
+            }
         }
 
-        for(int i=0; i<n; i++){
-            if(!isValid(board, occupied, row, i)) continue;
-            board[row][i] = true;
-            occupied[i] = true;
-            helper(res, board, occupied, row+1, n);
-            board[row][i] = false;
-            occupied[i] = false;
+        public boolean isValid(boolean[][] board, boolean[] occupied, int row, int col){
+
+            if(occupied[col]) return false;
+
+            for(int i=1; row-i>=0 && col-i>=0; i++){
+                if(board[row-i][col-i]) return false;
+            }
+            for(int i=1; row-i>=0 && col+i<board.length; i++){
+                if(board[row-i][col+i]) return false;
+            }
+
+            return true;
         }
-    }
-
-    public boolean isValid(boolean[][] board, boolean[] occupied, int row, int col){
-
-        if(occupied[col]) return false;
-
-        for(int i=1; row-i>=0 && col-i>=0; i++){
-            if(board[row-i][col-i]) return false;
-        }
-        for(int i=1; row-i>=0 && col+i<board.length; i++){
-            if(board[row-i][col+i]) return false;
-        }
-
-        return true;
     }
 }
